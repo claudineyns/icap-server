015aaee5d41a86d71504eed51754ca99e82e65f9
Claudiney Nascimento <contato@claudiney.dev>
2023-01-09 23:16:56 -0300

    Enhancements

--- src/main/java/io/github/rfc3507/server/Worker.java
+++ src/main/java/io/github/rfc3507/server/Worker.java
@@ -13,14 +13,17 @@ public class Worker {
 	public static void main(String[] args) throws IOException {
 		new Worker().start();
 	}
-	
+
 	private final Logger logger = Logger.getGlobal();
-	
+
 	private ServerSocket server;
-	
-	private void start() throws IOException {		
-		final Thread shutdown = new Thread(()->{
-			try { server.close(); } catch(IOException e) {}
+
+	private void start() throws IOException {
+		final Thread shutdown = new Thread(() -> {
+			try {
+				server.close();
+			} catch (IOException e) {
+			}
 			logger.info("[ICAP-SERVER] Service terminated.");
 		});
 		Runtime.getRuntime().addShutdownHook(shutdown);
@@ -31,7 +34,7 @@ public class Worker {
 	private void startService() {
 		try {
 			listen();
-		} catch(IOException e) {
+		} catch (IOException e) {
 			stopService();
 		}
 	}
@@ -39,31 +42,32 @@ public class Worker {
 	private void stopService() {
 		try {
 			server.close();
-		} catch(IOException e) {}
+		} catch (IOException e) {
+		}
 	}
 
 	private void listen() throws IOException {
 
 		final String servicePort = Optional
-			.ofNullable(System.getenv("APP_SERVICE_PORT"))
-			.orElse("1344");
+				.ofNullable(System.getenv("APP_SERVICE_PORT"))
+				.orElse("1344");
 
 		this.server = new ServerSocket(Integer.parseInt(servicePort));
-		
-		logger.info("[ICAP-SERVER] Listening on port "+servicePort);
-		
-		while(true) {
+
+		logger.info("[ICAP-SERVER] Listening on port " + servicePort);
+
+		while (true) {
 			Socket client = null;
 			try {
 				client = server.accept();
 				Logger.getGlobal().info("[ICAP-SERVER] Connection received!");
-			} catch(IOException e) {
+			} catch (IOException e) {
 				break;
 			}
 
 			CompletableFuture.runAsync(new ClientHandler(client));
 		}
-		
+
 	}
-	
+
 }
